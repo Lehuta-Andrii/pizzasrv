1827b163508daa6a9741cec78235f68845646a11
Andrii Lehuta <Andrii_Lehuta@epam.com>
2016-04-14 16:50:13 +0300

    Improved beanchmark annotation processing

--- src/main/java/org/study/pizzaservice/infrastructure/BeanchMarkProxyBeanPostProcessor.java
+++ src/main/java/org/study/pizzaservice/infrastructure/BeanchMarkProxyBeanPostProcessor.java
@@ -6,33 +6,41 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.aop.TargetSource;
-import org.springframework.aop.framework.Advised;
-import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class BeanchMarkProxyBeanPostProcessor implements BeanPostProcessor {
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String arg1) throws BeansException {
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String arg1) throws BeansException {
 
-	List<Class<?>> interfaces = new ArrayList<Class<?>>(Arrays.asList(bean.getClass().getInterfaces()));
-	interfaces.addAll(Arrays.asList(bean.getClass().getSuperclass().getInterfaces()));
+		for (Method method : bean.getClass().getMethods()) {
+			if (method.isAnnotationPresent(BeanchMark.class)) {
+				return Proxy.newProxyInstance(this.getClass().getClassLoader(), getAllInterfaces(bean.getClass()),
+						new BeanchMarkHandler(bean));
+			}
+		}
 
-	for (Method method : bean.getClass().getMethods()) {
-	    if (method.isAnnotationPresent(BeanchMark.class)) {
-		return Proxy.newProxyInstance(this.getClass().getClassLoader(), interfaces.toArray(new Class<?>[0]),
-			new BeanchMarkHandler(bean));
-	    }
+		return bean;
 	}
 
-	return bean;
-    }
+	private Class<?>[] getAllInterfaces(Class<?> bean) {
 
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String arg1) throws BeansException {
-	return bean;
-    }
+		if (bean.equals(Object.class)) {
+			return new Class<?>[0];
+		}
+
+		List<Class<?>> result = new ArrayList<Class<?>>(Arrays.asList(bean.getInterfaces()));
+
+		result.addAll(Arrays.asList(getAllInterfaces(bean.getSuperclass())));
+
+		return result.toArray(new Class<?>[0]);
+
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String arg1) throws BeansException {
+		return bean;
+	}
 
 }
