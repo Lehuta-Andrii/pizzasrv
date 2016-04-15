35b4eeb8b4a0e1cc173d549fe89d21a4c1b2e939
Andrii Lehuta <Andrii_Lehuta@epam.com>
2016-04-15 18:43:39 +0300

    added sql queries for postgre

---
+++ src/main/resources/pizzarepInsert.sql
@@ -0,0 +1,4 @@
+﻿insert into pizzas(name, price, pizzaType) values ('Margarita', 50, 'meat');
+insert into pizzas(name, price, pizzaType) values ('Americana', 76, 'sea');
+insert into pizzas(name, price, pizzaType) values ('Chilly', 50, 'vegetarian');
+
---
+++ src/main/resources/pizzzarep.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE PizzaType AS ENUM ('meat', 'sea', 'vegetarian');
+create table Pizzas(
+ id SERIAL PRIMARY KEY NOT NULL,
+ name TEXT NOT NULL,
+ price REAL NOT NULL,
+ pizzaType PizzaType 
+)
