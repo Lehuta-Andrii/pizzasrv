94b583b89440c576ee3df08bb67a5928376e3484
user <user@user-PC>
2016-04-25 22:23:13 +0300

    pizza fix

--- src/main/java/org/study/pizzaservice/PizzaApp.java
+++ src/main/java/org/study/pizzaservice/PizzaApp.java
@@ -20,64 +20,100 @@ import org.study.pizzaservice.service.PizzaShopTemplate;
 
 public class PizzaApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	Order order;
+		Order order;
 
-	ConfigurableApplicationContext repositoryContext = new ClassPathXmlApplicationContext("RepositoryContext.xml");
-	ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(
-		new String[] { "ApplicationContext.xml" }, false);
+		ConfigurableApplicationContext repositoryContext = new ClassPathXmlApplicationContext("RepositoryContext.xml");
+		ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				new String[] { "ApplicationContext.xml" }, false);
 
-	applicationContext.setParent(repositoryContext);
-	applicationContext.refresh();
+		applicationContext.setParent(repositoryContext);
+		applicationContext.refresh();
 
-//	PizzaShopTemplate pizzaShop = (PizzaShopTemplate) applicationContext.getBean(PizzaShopTemplate.class);
-//	CustomerService customers = (CustomerService) applicationContext.getBean(CustomerService.class);
 
-	CustomerRepository customers = applicationContext.getBean(CustomerRepository.class);
-	AccumulativeCardRepository accrep = applicationContext.getBean(AccumulativeCardRepository.class);
-	
-//	Customer customer = new Customer();
-//	
-//	List<Address> addr = new ArrayList<Address>();
-//	addr.add(new Address("Kiev", "Mura", "17","6", "555322"));
-//	addr.add(new Address("Kiev", "Bandera", "18","6", "555644"));
-//	customer.setAdresses(addr);
-//	
-//	customer.setName("Petro");
-//	
-//	AccumulativeCard aCard = new AccumulativeCardImpl();
-//	aCard.setCustomer(customers.getCostumerById(12l).get());
-//	
-//	accrep.addCard(aCard);
-//	
-//	AccumulativeCardService cardService = (AccumulativeCardService) applicationContext
-//		.getBean(AccumulativeCardService.class);
-//
-//	if(cardService.setNewCard(customers.getCostumerById(0))){
-//	    cardService.addSumToCard(customers.getCostumerById(0), 100);
-//	}
-//
-//	order = pizzaShop.makeOrder(customers.getCostumerById(0), 1l, 2l, 3l, 1l);
-//
-//	System.out.println(order);
-//	System.out.println(order.getPrice());
-//
-//	System.out.println(pizzaShop.getDiscount(order));
-//
-//	pizzaShop.accomplishOrder(order);
-//	System.out.println(order.getState());
-//
-//	
-//	//System.out.println(repositoryContext.getBean(PizzaRepository.class).getPizzas());
-//	repositoryContext.getBean(PizzaRepository.class).addPizza(new Pizza(null, "Bavaria", 105, Pizza.Type.MEAT));
-//		
-	System.out.println(repositoryContext.getBean(PizzaRepository.class).getPizzas());
-//	
-	
-	
-	repositoryContext.close();
-	applicationContext.close();
-    }
+		
+		
+		
+		// PizzaShopTemplate pizzaShop = (PizzaShopTemplate)
+		// applicationContext.getBean(PizzaShopTemplate.class);
+		// CustomerService customers = (CustomerService)
+		// applicationContext.getBean(CustomerService.class);
+
+		//CustomerRepository customers = applicationContext.getBean(CustomerRepository.class);
+		//AccumulativeCardRepository accrep = applicationContext.getBean(AccumulativeCardRepository.class);
+
+		// Customer customer = new Customer();
+		//
+		// List<Address> addr = new ArrayList<Address>();
+		// addr.add(new Address("Kiev", "Mura", "17","6", "555322"));
+		// addr.add(new Address("Kiev", "Bandera", "18","6", "555644"));
+		// customer.setAdresses(addr);
+		//
+		// customer.setName("Petro");
+		//
+		// AccumulativeCard aCard = new AccumulativeCardImpl();
+		// aCard.setCustomer(customers.getCostumerById(12l).get());
+		//
+		// accrep.addCard(aCard);
+		//
+		// AccumulativeCardService cardService = (AccumulativeCardService)
+		// applicationContext
+		// .getBean(AccumulativeCardService.class);
+		//
+		// if(cardService.setNewCard(customers.getCostumerById(0))){
+		// cardService.addSumToCard(customers.getCostumerById(0), 100);
+		// }
+		//
+		// order = pizzaShop.makeOrder(customers.getCostumerById(0), 1l, 2l, 3l,
+		// 1l);
+		//
+		// System.out.println(order);
+		// System.out.println(order.getPrice());
+		//
+		// System.out.println(pizzaShop.getDiscount(order));
+		//
+		// pizzaShop.accomplishOrder(order);
+		// System.out.println(order.getState());
+		//
+		//
+		// //System.out.println(repositoryContext.getBean(PizzaRepository.class).getPizzas());
+		// repositoryContext.getBean(PizzaRepository.class).addPizza(new
+		// Pizza(null, "Bavaria", 105, Pizza.Type.MEAT));
+		//
+		
+		PizzaRepository pizzaRepository = applicationContext.getBean(PizzaRepository.class);
+		
+		
+		//addPizzas(pizzaRepository);
+		
+		
+		System.out.println(pizzaRepository.getPizzas());
+		
+		
+		Pizza somePizza = pizzaRepository.getPizzaByID(8l).get();
+		somePizza.setPrice(400);
+		pizzaRepository.updatePizza(somePizza);
+		
+		System.out.println(pizzaRepository.getPizzas());
+		
+		
+		//pizzaRepository.deletePizza(pizzaRepository.getPizzaByID(4l).get());
+		
+
+		repositoryContext.close();
+		applicationContext.close();
+	}
+
+	public static void addPizzas(PizzaRepository pizzaRepository) {
+		pizzaRepository.addPizza(new Pizza("Margarita", 50, Pizza.Type.MEAT));
+		pizzaRepository.addPizza(new Pizza("Americana", 76, Pizza.Type.SEA));
+		pizzaRepository.addPizza(new Pizza("Chilly", 50, Pizza.Type.VEGETARIAN));
+		pizzaRepository.addPizza(new Pizza("Bavaria", 105, Pizza.Type.MEAT));
+	}
+
+	static void addCostumer(CustomerRepository rep) {
+
+	}
 
 }
--- src/main/java/org/study/pizzaservice/domain/Pizza.java
+++ src/main/java/org/study/pizzaservice/domain/Pizza.java
@@ -16,162 +16,168 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="pizzas")
+@Table(name = "pizzas")
 public class Pizza {
 
-    public enum Type {
-	VEGETARIAN, SEA, MEAT;
-    }
-
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name="id", nullable = false)
-    private Long id;
-  
-    @Column(name="name")
-    private String name;
-    
-    @Column(name="price")
-    private Double price;
-    
-    @Enumerated(EnumType.STRING)
-    @Column(name="pizzatype")
-    private Type pizzaType;
-
-    protected Pizza(){
-    }
-    
-    public Pizza(Long id, String name, double price, Type pizzaType) {
-
-	this.id = id;
-	this.name = name;
-	this.price = price;
-	this.pizzaType = pizzaType;
-    }
-
-    /**
-     * @return the id
-     */
-    public Long getId() {
-	return id;
-    }
-
-    /**
-     * @param id
-     *            the id to set
-     */
-    public void setId(Long id) {
-	this.id = id;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-	return name;
-    }
-
-    /**
-     * @param name
-     *            the name to set
-     */
-    public void setName(String name) {
-	this.name = name;
-    }
-
-    /**
-     * @return the price
-     */
-    public double getPrice() {
-	return price;
-    }
-
-    /**
-     * @param price
-     *            the price to set
-     */
-    public void setPrice(double price) {
-	this.price = price;
-    }
-
-    /**
-     * @return the pizzaType
-     */
-    public Type getPizzaType() {
-	return pizzaType;
-    }
-
-    /**
-     * @param pizzaType
-     *            the pizzaType to set
-     */
-    public void setPizzaType(Type pizzaType) {
-	this.pizzaType = pizzaType;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((id == null) ? 0 : id.hashCode());;
-	result = prime * result + ((name == null) ? 0 : name.hashCode());
-	result = prime * result + ((pizzaType == null) ? 0 : pizzaType.hashCode());
-	result = prime * result + ((price == null) ? 0 : price.hashCode());
-	return result;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj) {
-	    return true;
+	public enum Type {
+		VEGETARIAN, SEA, MEAT;
 	}
-	if (obj == null) {
-	    return false;
-	}
-	if (getClass() != obj.getClass()) {
-	    return false;
-	}
-	Pizza other = (Pizza) obj;
-	if (id.compareTo(other.id) != 0) {
-	    return false;
-	}
-	if (name == null) {
-	    if (other.name != null) {
-		return false;
-	    }
-	} else if (!name.equals(other.name)) {
-	    return false;
-	}
-	if (pizzaType != other.pizzaType) {
-	    return false;
-	}
-	if (price == null) {
-	    if (other.price != null) {
-		return false;
-	    }
-	} else if (!price.equals(other.price)) {
-	    return false;
-	}
-	return true;
-    }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-	return "Pizza [id=" + id + ", name=" + name + ", price=" + price + ", pizzaType=" + pizzaType + "]";
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "price")
+	private Double price;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "pizzatype")
+	private Type pizzaType;
+
+	protected Pizza() {
+	}
+
+	public Pizza(Long id, String name, double price, Type pizzaType) {
+
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.pizzaType = pizzaType;
+	}
+
+	public Pizza(String name, double price, Type pizzaType) {
+		this(null, name, price, pizzaType);
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price
+	 *            the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the pizzaType
+	 */
+	public Type getPizzaType() {
+		return pizzaType;
+	}
+
+	/**
+	 * @param pizzaType
+	 *            the pizzaType to set
+	 */
+	public void setPizzaType(Type pizzaType) {
+		this.pizzaType = pizzaType;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((pizzaType == null) ? 0 : pizzaType.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Pizza other = (Pizza) obj;
+		if (id.compareTo(other.id) != 0) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (pizzaType != other.pizzaType) {
+			return false;
+		}
+		if (price == null) {
+			if (other.price != null) {
+				return false;
+			}
+		} else if (!price.equals(other.price)) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Pizza [id=" + id + ", name=" + name + ", price=" + price + ", pizzaType=" + pizzaType + "]";
+	}
 
 }
